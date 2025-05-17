d1fc25d23de6d02c45f66c10bb1f8514ffb90670
smattia-rizzo <192420956+smattia-rizzo@users.noreply.github.com>
2025-05-17 12:58:25 +0200

    Modifiche varie - Gestione titoli delle pagine - Creazione di nuove Classi Utente, Quiz e Domanda - Risolto il bug del timer che si avviava immediatamente al caricamento del sito
    Co-authored-by: barba-12 <barba-12@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,17 +1,224 @@
 /**
- * Funzione per cambiare la pagina in base all'hash dell'url
+ * Una classe per gestire l'utente all'interno del sito web
  */
+class Utente {
+  /**
+   * 
+   * @param {string} nome Nome Utente
+   * @param {Quiz} quizCorrente Quiz effettuato
+   */
+  constructor(nome, quizCorrente) {
+    this.nome = nome;
+    this.quizCorrente = quizCorrente;
+  }
+  /**
+   * Ritorna il nome utente
+   * @returns {string}
+   */
+  getNome(){
+    return this.nome;
+  }
+  /**
+   * Setta il nome utente
+   * @param {*} nome Nome Utente
+   */
+  setNome(nome){
+    this.nome = nome;
+  }
+
+  /**
+   * Ritorna il Quiz corrente
+   * @returns {Quiz}
+   */
+  getQuizCorrente(){
+    return this.quizCorrente;
+  }
+
+  /**
+   * Setta il Quiz corrente
+   * @param {Quiz} quizCorrente 
+   */
+  setQuizCorrente(quizCorrente){
+    this.quizCorrente = quizCorrente;
+  }
+}
+
+/**
+ * Classe per gestire il Quiz del sito web
+ */
+class Quiz {
+  /**
+   * Crea il Quiz con le domande e il timer
+   * @param {Domanda[]} domande Array di domande
+   * @param {Timer} timer Timer
+   */
+  constructor(domande, timer) {
+    this.domande = domande;
+    this.timer = timer;
+  }
+
+  /**
+   * Ritorna la lista di domande per il Quiz
+   * @returns {Domanda[]}
+   */
+  getDomande(){
+    return this.domande;
+  }
+  /**
+   * Setta le domande
+   * @param {Domanda[]} domande Array di domande
+   */
+  setDomande(domande){
+    this.domande = domande;
+  }
+
+  /**
+   * Ritorna il Timer del Quiz
+   * @returns {Timer}
+   */
+  getTimer(){
+    return this.timer;
+  }
+  /**
+   * Setta il Timer del Quiz
+   * @param {Timer} timer Timer
+   */
+  setTimer(timer){
+    this.timer = timer;
+  }
+}
+
+/**
+ * Classe adibita per le domande e la loro logica
+ */
+class Domanda {
+  constructor(testo, img, indice, risposta) {
+    this.testo = testo;
+    this.img = img;
+    this.indice = indice;
+    this.risposta = risposta;
+    this.rispUtente = null;
+  }
+
+  getTesto(){
+    return this.testo;
+  }
+  setTesto(testo){
+    this.testo = testo;
+  }
+
+  getImg(){
+    return this.img;
+  }
+  setImg(img){
+    this.img = img;
+  }
+  
+  getIndice(){
+    return this.indice;
+  }
+  setIndice(indice){
+    this.indice = indice;
+  }
+
+  getRisposta(){
+    return this.risposta;
+  }
+  setRisposta(risposta){
+    this.risposta = risposta;
+  }
+
+  getRispUtente(){
+    return this.rispUtente;
+  }
+  setRispUtente(rispUtente){
+    this.rispUtente = rispUtente;
+  }
+}
+
+class Timer {
+  constructor(secondi) {
+    this.secondi = secondi;
+    this.tempoRimasto = secondi;
+    this.timerElement = document.getElementById('timer');
+    this.aggiornaTimer();
+    this.timerInterval = setInterval(this.aggiornaTimer.bind(this), 1000)
+  }
+
+  aggiornaTimer() {
+    const minuti = Math.floor(this.tempoRimasto / 60);
+    const secondi = this.tempoRimasto % 60;
+
+    // Formatta in mm:ss (esempio 05:09)
+    this.timerElement.textContent = `Tempo rimasto: ${minuti.toString().padStart(2, '0')}:${secondi.toString().padStart(2, '0')}`;
+
+    if (this.tempoRimasto === 0) {
+      clearInterval(this.timerInterval);
+      // Tempo scaduto, azioni da fare
+      alert('Il tempo è scaduto!');
+      // Per esempio puoi bloccare i bottoni o terminare il quiz
+      document.getElementById('btnTrue').disabled = true;
+      document.getElementById('btnFalse').disabled = true;
+    } else {
+      this.tempoRimasto--;
+    }
+    //this.timerInterval = setInterval(this.aggiornaTimer(), 1000);
+    //timerInterval = setInterval(aggiornaTimer(), 1000);
+  }
+}
+
+let timer = null;
+const titoloSito = "Educazione Stradale";
+
+/**
+ * Funzione per cambiare la pagina in base all'hash dell'url
+*/
 function cambiaPagina() {
-      const nome_pagina = location.hash || "#pg0";
+      //Recupero informazioni necessarie per il cambio pagina
+      const nome_pagina = location.hash || "#pgHomepage";
       const pagine = document.querySelectorAll(".pagina"); //Seleziona tutti gli elementi pagina
 
+      //Gestione logica del cambio pagina
       pagine.forEach(pagina => {
         pagina.classList.remove("attiva");  //Deseleziona le pagine attualmente attive
         if ("#" + pagina.id === nome_pagina) {
           pagina.classList.add("attiva");   //Rende attiva solamente la pagina selezionata
         }
       });
+      
+      //Gestione del titolo del Sito
+      document.title = titoloSito;
+      switch (nome_pagina) {
+        case "#pgHomepage":
+          document.title += " - Homepage"
+          break;
+        case "#pg1":
+          document.title += " - definizioni-generali-doveri-strada"
+          break;
+        case "#pg2":
+          document.title += " - segnali stradali e di pericolo"
+          break;
+        case "#pg3":
+          document.title += " - limiti di velocità, pericolo e intralcio alla circolazione"
+          break;
+        case "#pg4":
+          document.title += " - esempi di precedenza"
+          break;
+        case "#pg5":
+          document.title += " - arresto, fermata e sosta"
+          break;
+        case "#pgQuizFinale":
+          document.title += " - Quiz Patente"
+          break;
+      }
+      
+      //Gestione Timer se la pagina è giusta
+      if(location.hash == "#pgQuizFinale"){
+        if (timer == null) {
+          timer = new Timer(20*60);
+        }
     }
+  } 
 
 // Pagina al caricamento del documento html
 window.addEventListener("load", cambiaPagina);
@@ -21,6 +228,9 @@ window.addEventListener("hashchange", cambiaPagina);
 
 let domandeSelezionate = [];
 let indiceDomandaCorrente = 0;
+//cambiare indice corrente in base alla domnada selezionata
+//richiamare function mostraDOmanda con l'idece corrente
+
 /**
  * Link raw del GitHub dove prendiamo le informazioni per il quiz
  */
@@ -31,7 +241,6 @@ const rawUrlGitHubQuiz = "https://raw.githubusercontent.com/Ed0ardo/QuizPatenteB
  * @todo ProgressBar
  * @todo Randomizzazione della pesca delle domande per il test finale
  * @todo Il Quiz Finale comprenderà tutti gli argomenti trattati, mentre quelli relativi alle pagine solo l'argomento stesso
- * @error Il Quiz viene avviato al caricamento generale invece del caricamento del div relativo al quiz
  */
   fetch(rawUrlGitHubQuiz + "quizPatenteB2023.json")
     .then(response => response.json())
@@ -47,36 +256,6 @@ const rawUrlGitHubQuiz = "https://raw.githubusercontent.com/Ed0ardo/QuizPatenteB
           }
         }
       }
-
-      const durataTimer = 20 * 60; // 20 minuti in secondi
-      let tempoRimasto = durataTimer;
-
-      const timerElement = document.getElementById('timer');
-
-      function aggiornaTimer() {
-        const minuti = Math.floor(tempoRimasto / 60);
-        const secondi = tempoRimasto % 60;
-
-        // Formatta in mm:ss (esempio 05:09)
-        timerElement.textContent = `Tempo rimasto: ${minuti.toString().padStart(2, '0')}:${secondi.toString().padStart(2, '0')}`;
-
-        if (tempoRimasto === 0) {
-          clearInterval(timerInterval);
-          // Tempo scaduto, azioni da fare
-          alert('Il tempo è scaduto!');
-          // Per esempio puoi bloccare i bottoni o terminare il quiz
-          document.getElementById('btnTrue').disabled = true;
-          document.getElementById('btnFalse').disabled = true;
-        } else {
-          tempoRimasto--;
-        }
-      }
-
-      const timerInterval = setInterval(aggiornaTimer, 1000);
-
-      // Avvia subito l'aggiornamento per mostrare subito 20:00 (invece di 19:59)
-      aggiornaTimer();
-
 
       // Mescola le domande e prendi le prime 30
       domandeSelezionate = tutteDomande.sort(() => 0.5 - Math.random()).slice(0, 30);
